d287e02d0e10f222c174e8df8629ead088139b5a
Sanjeet990 <sanjeet.pathak990@gmail.com>
2018-12-29 15:36:48 +0530

    Changed package name

---
+++ app/src/main/java/com/github/Sanjeet990/launcher3/OnAlarmListener.java
@@ -0,0 +1,5 @@
+package com.github.Sanjeet990.launcher3;
+
+public interface OnAlarmListener {
+    public void onAlarm(Alarm alarm);
+}
--- app/src/main/java/com/github/songjiubin/launcher3/OnAlarmListener.java
+++
@@ -1,5 +0,0 @@
-package com.github.songjiubin.launcher3;
-
-public interface OnAlarmListener {
-    public void onAlarm(Alarm alarm);
-}
